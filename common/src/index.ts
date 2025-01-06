a5dfa2b77c27525f10e4b3c45a276dff01992b88
nepalrohan <rohannepal098@gmail.com>
2025-01-06 21:16:30 +0545

    Added zod and type inference

---
+++ common/src/index.ts
@@ -0,0 +1,45 @@
+import {z} from 'zod';
+
+
+//signup
+export const signupInput = z.object({
+    email:z.string().email(),
+    password:z.string().min(6),
+    name:z.string().optional()
+})
+
+
+export type SignupInput = z.infer<typeof signupInput>
+
+
+
+//signin
+export const signinInput = z.object({
+    email:z.string().email(),
+    password:z.string().min(6),
+
+})
+
+
+export type SigninInput = z.infer<typeof signinInput>
+
+
+//createblog
+export const createBlogInput = z.object({
+    title:z.string(),
+    content:z.string()
+})
+
+export type CreateBlogInput = z.infer<typeof createBlogInput>
+
+
+
+//updateblog
+
+export const updateBlogInput = z.object({
+    title:z.string(),
+    content:z.string(),
+    id:z.string()
+})
+
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>
